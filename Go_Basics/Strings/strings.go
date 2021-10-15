bc699f67de314089bf67fd6310f6b8b9b5140c01
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-15 18:36:55 +0500

    some alterations

---
+++ Go_Basics/Strings/strings.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	book := "The color of magic"
+	fmt.Println(book)
+
+	fmt.Println(len(book))
+
+	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
+	// uint8 is byte
+}
