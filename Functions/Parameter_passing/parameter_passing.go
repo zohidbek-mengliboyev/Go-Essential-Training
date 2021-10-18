d1e97c83765f538a02e64b91a0e3e146156affce
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-18 15:55:30 +0500

    many changes

--- Functions/Parameter_passing/parameter_passing.go
+++ Functions/Parameter_passing/parameter_passing.go
@@ -8,8 +8,16 @@ func doubleAt(values []int, i int) {
 	values[i] *= 2
 }
 
+func double(n *int) {
+	*n *= 2
+}
+
 func main() {
 	values := []int{1, 2, 3, 4}
 	doubleAt(values, 2)
 	fmt.Println(values)
+
+	val := 10
+	double(&val)
+	fmt.Println(val)
 }
