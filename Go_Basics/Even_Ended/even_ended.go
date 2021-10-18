f0c55ec1aae8a28d1bdc88587d2417ccdea284c5
Zohidbek Mengliboyev <mzoxidbek@bk.ru>
2021-10-18 13:20:05 +0500

    strings and even-ended numbers were added

---
+++ Go_Basics/Even_Ended/even_ended.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// count = 0
+	count := 0
+
+	// for every pair of 4 digits numbers
+	for a := 1000; a <= 9999; a++ {
+		for b := a; b <= 9999; b++ { // don't count twice
+			n := a * b
+
+			// if a * b even ended
+			s := fmt.Sprintf("%d", n)
+			if s[0] == s[len(s)-1] {
+				// count = count + 1
+				count++
+			}
+		}
+	}
+
+	// print count
+	fmt.Println(count)
+
+}
--- Go_Basics/Strings/strings.go
+++ Go_Basics/Strings/strings.go
@@ -13,4 +13,22 @@ func main() {
 	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
 	// uint8 is byte
 	// this is my string task
+
+	// Strings in go are immutable
+	// book[0] = 116
+	fmt.Println(book[4:11])
+	fmt.Println(book[4:])
+	fmt.Println(book[:4])
+	fmt.Println("t" + book[1:])
+	poem := `
+	The road goes ever on
+	Down from the door where it began
+	...
+	`
+	fmt.Println(poem)
+
+	n := 42
+	s := fmt.Sprintf("%d", n)
+	fmt.Printf("s = %s (type %T)\n", s, s)
+	fmt.Printf("s = %q (type %T)\n", s, s)
 }
